01aaf8f57d623be3f8e39b241fd96a1260cccf90
Akzajohn1 <akzajohnrose9@gmail.com>
2019-04-05 10:33:05 -0700

    first change

--- socket/src/socket/Server.java
+++ socket/src/socket/Server.java
@@ -22,7 +22,8 @@ public class Server
             int port = 25000;
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Server Started and listening to the port 25000");
- 
+            System.out.println("_______________________________________________");
+
             //Server is running always. This is done using this while(true) loop
             while(true)
             {
